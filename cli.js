a2f00758c277756f34b1976b9a54b62085afdcb7
Joshua Karjala-Svendsen <joshua@fluxuries.com>
2016-03-02 19:44:07 +0100

    console logging of encrypt / decrypt

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 
 var argv = require('minimist')(process.argv.slice(2));
 
+// Generate a 256bit AES key
 if (argv._ !== undefined && argv._[0] === 'generate') {
   var randomstring = require('randomstring');
   console.log(randomstring.generate(33));
@@ -9,8 +10,17 @@ if (argv._ !== undefined && argv._[0] === 'generate') {
 }
 
 var key = argv.env;
+var write = argv.write
 
 var encryptEnv = require('./index')(key);
 
-if (argv.decrypt === true) encryptEnv.decryptEnv(argv.write);
-else encryptEnv.encryptEnv(argv.write);
+if (argv.decrypt === true) {
+  var d = encryptEnv.decryptEnv();
+  if(!write) console.log(d);
+}
+else {
+  var e = encryptEnv.encryptEnv(argv.write);
+  if(!write) console.log(e);
+}
+
+process.exit(0);

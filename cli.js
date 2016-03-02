818a226c1603ca3cae42b6d807e27c53f861022b
Joshua Karjala-Svendsen <joshua@fluxuries.com>
2016-03-02 19:23:14 +0100

    Add key generate function to CLI

--- cli.js
+++ cli.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
 var argv = require('minimist')(process.argv.slice(2));
+
+if (argv._ !== undefined && argv._[0] === 'generate') {
+  var randomstring = require('randomstring');
+  console.log(randomstring.generate(33));
+  process.exit(0);
+}
+
 var key = argv.env;
 
 var encryptEnv = require('./index')(key);
